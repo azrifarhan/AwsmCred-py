516f05ee793cf5be54dbbca7d0ecd456baf2bd01
azrifarhan <141860313+azrifarhan@users.noreply.github.com>
2025-10-15 01:01:20 +0800

    Update DID_Creation.py
    Knowing that the structure for all messages in the topic for service, verification method, relationship are the same dont need to depend on resolve() just the following:
    
    Query topic from topicid in did
    query message
    get latest message (need to find the best/optimal way like storing whole did in a did document, the logs only save in the topic which is what resolve is doing)
    extract event
    decode event
    
    use it decoded event to independently resolve did

--- Hedera_DID/DID_Creation.py
+++ Hedera_DID/DID_Creation.py
@@ -1,32 +1,75 @@
 from hiero_did_sdk_python import HederaDid
-from hiero_sdk_python import Network, Client, AccountId, PrivateKey
 import asyncio
-from dotenv import load_dotenv
-import os
-load_dotenv()
-operator_key = PrivateKey.from_string(os.getenv("OPERATOR_KEY"))
-der_privkey = operator_key.to_string()
-public_key = PublicKey.from_string(os.getenv("PUBLIC_KEY"))
-der_pubkey = public_key.to_string()
-network = Network(network='testnet')
-client = Client(network)
-operator_id = AccountId.from_string(os.getenv('OPERATOR_ID'))
-client.set_operator(operator_id, operator_key)
-did = HederaDid(client=client, private_key_der=der_privkey)
+
+from hed_test.Client_set import (
+    operator_id, 
+    operator_key, 
+    der_privkey, 
+    der_pubkey,
+    issuer_id,
+    der_pubkey_issuer,
+    der_privkey_issuer,
+    issuer_key, 
+    client)
+
+
+client.set_operator(issuer_id, issuer_key)
+did = HederaDid(client=client, private_key_der=der_privkey_issuer)
 
 async def main():
     await did.register()
     print(f"✅ DID registered: {did.identifier}")
+    """
+sample result
+    
+{
+  "message": {
+    "timestamp": 1760460388.2378209,
+    "operation": "create",
+    "did": "did:hedera:testnet:zG7LDyyCrmV3eFSDU5Z76Fkhqf1tTmqiyJcUDh36Upggq_0.0.7058254",
+    "event": "eyJWZXJpZmljYXRpb25NZXRob2QiOiB7ImlkIjogImRpZDpoZWRlcmE6dGVzdG5ldDp6RzdMRHl5Q3JtVjNlRlNEVTVaNzZGa2hxZjF0VG1xaXlKY1VEaDM2VXBnZ3FfMC4wLjcwNTgyNTQja2V5LTEiLCAidHlwZSI6ICJFZDI1NTE5VmVyaWZpY2F0aW9uS2V5MjAxOCIsICJjb250cm9sbGVyIjogImRpZDpoZWRlcmE6dGVzdG5ldDp6RzdMRHl5Q3JtVjNlRlNEVTVaNzZGa2hxZjF0VG1xaXlKY1VEaDM2VXBnZ3FfMC4wLjcwNTgyNTQiLCAicHVibGljS2V5QmFzZTU4IjogIjIzZHREU0RFOEU0OWdIc0NydHRHMkdVV0FScG5vRFIyYXFWYzVuTDRLWXVqIn19"
+  },
+  "signature": "signature hash"}
+
+  event decoded:
+
+  {"DIDOwner": {
+  "id": "did:hedera:testnet:zG7LDyyCrmV3eFSDU5Z76Fkhqf1tTmqiyJcUDh36Upggq_0.0.7058254#did-root-key", 
+  "type": "Ed25519VerificationKey2018", 
+  "controller": "did:hedera:testnet:zG7LDyyCrmV3eFSDU5Z76Fkhqf1tTmqiyJcUDh36Upggq_0.0.7058254", 
+  "publicKeyBase58": "G7LDyyCrmV3eFSDU5Z76Fkhqf1tTmqiyJcUDh36Upggq"}}      
+    
+    """
+
+
     await did.add_service(
         id_=f"{did.identifier}#service-1", service_type="LinkedDomains", service_endpoint="https://example.com/vcs"
     )
+
+    """event decoded
+    
+    {"Service": {
+    "id": "did:hedera:testnet:zG7LDyyCrmV3eFSDU5Z76Fkhqf1tTmqiyJcUDh36Upggq_0.0.7058254#service-1", 
+    "type": "LinkedDomains", 
+    "serviceEndpoint": "https://example.com/vcs"}}
+    
+    """
     await did.add_verification_method(
         id_=f"{did.identifier}#key-1",controller=did.identifier,public_key_der=der_pubkey,type_="Ed25519VerificationKey2018")
+    
+    """
+    event decoded
+    {"VerificationMethod": {
+    "id": "did:hedera:testnet:zG7LDyyCrmV3eFSDU5Z76Fkhqf1tTmqiyJcUDh36Upggq_0.0.7058254#key-1", 
+    "type": "Ed25519VerificationKey2018", "controller": "did:hedera:testnet:zG7LDyyCrmV3eFSDU5Z76Fkhqf1tTmqiyJcUDh36Upggq_0.0.7058254", 
+    "publicKeyBase58": "23dtDSDE8E49gHsCrttG2GUWARpnoDR2aqVc5nL4KYuj"}}
 
-    #For some reason this works but no the HederaDidResolver
-    await asyncio.sleep(5)
-    did_document = await did.resolve()
-    did_document_dict = did_document.get_json_payload()
-    print(did_document_dict)
+    
+    """
+
+    await asyncio.sleep(20)
+
+
+    print(did.topic_id)
 
 asyncio.run(main())
